4e1a50e46074b0dce7c2ce39283ad86fe3d783d6
pulkitsrivastava9 <pulkitsrivastava9@gmail.com>
2021-05-26 14:35:06 +0400

    from local

--- API/Startup.cs
+++ API/Startup.cs
@@ -46,6 +46,15 @@ namespace API
 
             });
 
+            services.AddCors(opt =>
+            {
+
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,6 +70,7 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
